af3f82a7a5874667f4441ff0c6bdb01105eb8242
himmir00 <himmiraja7@gmail.com>
2025-10-04 15:44:50 +0530

    eslint and other security checks added

---
+++ .eslint.config.js
@@ -0,0 +1,37 @@
+// eslint.config.js
+import js from "@eslint/js";
+import prettier from "eslint-plugin-prettier";
+
+export default [
+  {
+    languageOptions: {
+      globals: {
+        // Node.js globals
+        require: "readonly",
+        module: "readonly",
+        __dirname: "readonly",
+        console: "readonly",
+      },
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    plugins: {
+      prettier,
+    },
+    linterOptions: {
+      reportUnusedDisableDirectives: "error",
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      "prettier/prettier": "error",
+      "no-console": "warn",
+      "no-unused-vars": "error",
+      "prefer-const": "warn",
+      "eqeqeq": ["error", "always"],
+      "curly": ["error", "all"],
+      "semi": ["error", "always"],
+    },
+  },
+];
+
+ 
