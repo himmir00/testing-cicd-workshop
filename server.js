42a6ea1e08de34edceacc006b8541722f98a7fbc
himmir00 <himmiraja7@gmail.com>
2025-10-04 12:45:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,1 @@
-const { app } = require('./app');
 
-const port = process.env.PORT || 3000;
-
-const server = app.listen(port, () => {
-    console.log(`Calculator API listening at http://localhost:${port}`);
-});
-
-module.exports = server;
